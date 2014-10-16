47fdb9e12bcf920e9ed3253cb0d5b09a0d658c17
iiyajid <issame.yajid@gmail.com>
2014-10-17 00:39:55 +0200

    commit de l'exercice 8

---
+++ ex8/donnees.h
@@ -0,0 +1,12 @@
+/* ===================================================================== *
+ *
+ * header donnees.h
+ * 
+ * ===================================================================== */
+
+/* declaration des fonctions definies dans donnees.c */
+
+extern void Lire(ITEM items[], int *nb_items);
+extern void Afficher(ITEM items[], int nb_items);
+
+/* ===================================================================== */
